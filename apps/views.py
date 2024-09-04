edf65fff5fcd3927bd7d9487dc918d856628ec57
krv006 <rvkamronbek@gmail.com>
2024-09-04 15:11:55 +0500

    add

--- apps/views.py
+++ apps/views.py
@@ -1,8 +1,1 @@
 from django.shortcuts import render
-
-
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/krv006/P23_DRF.git
-git push -u origin main
